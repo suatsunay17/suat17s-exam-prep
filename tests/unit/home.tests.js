5e0c1e22425ea04021b5357887250913c4877f2a
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2023-12-12 19:18:05 +0200

    change urls

--- tests/unit/home.tests.js
+++ tests/unit/home.tests.js
@@ -3,13 +3,13 @@ const fetch = require('node-fetch');
 
 suite('Home page', function() {
   test('Page title', async function() {
-    let res = await fetch("http://localhost:8888/");
+    let res = await fetch("https://suat17s.onrender.com/");
     let body = await res.text();
     assert.ok(body.includes("<h1>Boardgames Collection</h1>"));
   });
   
   test('Boardgames count', async function() {
-    let res = await fetch("http://localhost:8888/");
+    let res = await fetch("https://suat17s.onrender.com/");
     let body = await res.text();
     assert.ok(body.includes("Added boardgames: <b>3</b>"));
   });
